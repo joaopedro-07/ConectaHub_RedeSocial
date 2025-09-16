5038d7b1287d6b4d58c5702f5a31f54d354eb788
joaopedro-07 <jotapepe.machado@gmail.com>
2025-09-16 13:43:09 -0300

    nav

--- php/feed.php
+++ php/feed.php
@@ -1,13 +1,36 @@
-<!-- FEED/INICIO -->
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/feed.css">
     <title>Feed</title>
 </head>
 <body>
-    <h1>Teste</h1>
+    <header>
+        <nav>
+            <img src="../img/conectahub_logoFundoAzul.png" alt="">
+            <ul>
+                <li><a href="../php/feed.php">Feed</a></li>
+                <li><a href="../php/chat.php">Mensagens</a></li>
+                <li><a href="../php/perfil.php">Perfil</a></li>
+            </ul>
+            <h3>Sair</h3>
+        </nav>
+    </header>
+    <main>
+        <h1>Nova Publicação</h1>
+        <form action="" method="post">
+            <input type="text">
+        </form>
+        <div class="post">
+            <div class="autor">
+                <img src="../img/palmeiras_escudo.png" alt="">
+                <h4>opalmeirense</h4>
+            </div>
+            <img class="img-post" src="../img/vitor_roque.png" alt="">
+        </div>
+    </main>
+    <footer></footer>
 </body>
 </html>
